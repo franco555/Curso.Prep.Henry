eb9dcdff0e4ade91d9a3bfbf79a8795ba58542b5
franco555 <razon20m@gmail.com>
2021-06-18 19:58:29 -0300

    resolucion 07-JS-VI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,29 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  let ne="";
+  for(var index = 0; index < nombre.length ; index++) {
+    if(index==0){
+      ne=ne + nombre[index].toUpperCase();
+    }else{
+      ne=ne + nombre[index]
+    }
+  }
+
+  return ne;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +34,19 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var total= numeros.reduce(function(acumulado, actual){
+    return acumulado+actual;
+  },0);
+  return cb(total);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(element => {
+    cb(element);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +54,25 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var n=[];
+  array.map(function(e){
+    n.push(cb(e));
+  });
+  return n;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  var na=[];
+  array.map(function(e){
+    if(e.charAt(0)==="a"){
+      na.push(e);
+    }
+  });
+
+  return na;
 }
 
 // No modificar nada debajo de esta línea
